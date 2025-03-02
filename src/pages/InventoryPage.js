dbbb13279ff0dbc9bed6cf1288f21c52f775c1ec
Michal <m.jagodzinski1234@gmail.com>
2025-03-02 13:29:03 +0100

    KAN-19 header and scaninputsection

---
+++ src/pages/InventoryPage.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header';
+import ScanInputSection from '../components/ScanInputSection';
+import './InventoryPage.css';
+
+
+function InventoryPage(){
+    return(
+        <div>
+            <Header />
+            <ScanInputSection />
+        </div>
+    );
+}
+
+export default InventoryPage;

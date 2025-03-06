d9350702c3b930f1793d5551cb7a078293c8c46d
Michal <m.jagodzinski1234@gmail.com>
2025-03-06 20:34:25 +0100

    KAN-22 header icon added

--- src/pages/InventoryPage.js
+++ src/pages/InventoryPage.js
@@ -2,12 +2,18 @@ import Header from '../components/Header';
 import ReactTable from '../components/ReactTable';
 import ScanInputSection from '../components/ScanInputSection';
 import './InventoryPage.css';
-
+import myIcon from '../icons/back.png';
 
 function InventoryPage(){
     return(
     <div>
-        <Header />
+        <div className='header'>
+            <img
+                src={myIcon} 
+                alt="Back icon" 
+            />
+            <Header />
+        </div>
         <div className='inventory-page'>
             <ScanInputSection />
             <ReactTable />

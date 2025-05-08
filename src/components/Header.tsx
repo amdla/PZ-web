7371033f694aa64f91389b9a0563bbe477f58b04
Jedrek Dubiak <jedrzejdubiak@gmail.com>
2025-05-08 17:50:41 +0200

    KAN-21 TypeScript added in app

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header()  {
+  return (
+    <header className="page-header">
+      <div className='flex-row'>
+        <p>Zalogowano jako xyz@pw.edu.pl</p>
+        <button className='black-button' id='logout'>Wyloguj</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/InventoryPage.tsx
+++ src/pages/InventoryPage.tsx
@@ -6,14 +6,32 @@ import { useNavigate } from 'react-router-dom';
 import './InventoryPage.css';
 import myIcon from '../icons/back.png';
 
+interface InventoryItem {
+  id: number;
+  inventory: number;
+  department: number;
+  asset_group: number;
+  category: string;
+  inventory_number: string;
+  asset_component: number;
+  sub_number: number;
+  acquisition_date: string;
+  asset_description: string;
+  quantity: number;
+  initial_value: string;
+  room: string;
+  new_room: string;
+  scanned: boolean;
+}
+
 function InventoryPage() {
   const navigate = useNavigate();
-  const [tableData, setTableData] = useState([]);
+  const [tableData, setTableData] = useState<InventoryItem[]>([]);
 
   useEffect(() => {
     fetch('/inventoryData.json')
       .then((response) => response.json())
-      .then((data) => setTableData(data))
+      .then((data: InventoryItem[]) => setTableData(data))
       .catch((error) => console.error("Error loading JSON:", error));
   }, []);
 
@@ -26,7 +44,7 @@ function InventoryPage() {
           onClick={() => navigate('/')}
           style={{ cursor: 'pointer' }}
         />
-        <Header />
+        <Header />  
       </div>
       <div className='inventory-page'>
         <ScanInputSection tableData={tableData} setTableData={setTableData} />

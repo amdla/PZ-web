7ef046710cfb2e3720313bfdb0a0c7d16653b027
Michal <m.jagodzinski1234@gmail.com>
2025-05-25 14:54:22 +0200

    KAN-53 login done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import InventoryPage from './pages/InventoryPage';
-import LoginPage from './pages/LoginPage'; // Importuj LoginPage
-import { AuthProvider } from './contexts/AuthContext'; // Importuj AuthProvider
-import ProtectedRoute from './components/ProtectedRoute'; // Importuj ProtectedRoute
+import LoginPage from './pages/LoginPage';
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    // 1. Owiń wszystko w AuthProvider
     <AuthProvider>
       <Routes>
-        {/* 2. Dodaj trasę do strony logowania - dostępna dla wszystkich */}
         <Route path="/login" element={<LoginPage />} />
-
-        {/* 3. Utwórz grupę tras chronionych */}
         <Route element={<ProtectedRoute />}>
-          {/* Wszystkie trasy wewnątrz będą chronione */}
           <Route path="/" element={<MainPage />} />
           <Route path="/inventory" element={<InventoryPage />} />
-          {/* Możesz tu dodać więcej chronionych tras */}
         </Route>
       </Routes>
     </AuthProvider>
